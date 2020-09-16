5ca934692338625c87e59407f702dd8051367fa2
klastine <klastine@DESKTOP-22EUOPV.localdomain>
2020-09-16 00:22:16 -0500

    fixed bug where arm_control.py wouldn't start

--- ROS Nodes/arm_control.py
+++ ROS Nodes/arm_control.py
@@ -3,24 +3,21 @@ import rospy
 from std_msgs.msg import Int16
 
 def talker():
-
     pub_base = rospy.Publisher('base',Int16,queue_size=10)
     
     pub_shoulder = rospy.Publisher('shoulder',Int16,queue_size=10)
 
     pub_elbow = rospy.Publisher('elbow',Int16,queue_size=10)
 
-    rospy.init_node('wheel_control')
+    rospy.init_node('arm_control')
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-
-        #publish to base, shoulder, elbow
-
+        
         rate.sleep()
 
-    if __name__ == '__main__':
-        try:
-            talker()
-        except rospy.ROSInterruptException:
-            pass
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
