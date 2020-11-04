053879b92efa7a06afb207f5eed0887238b7d56c
rutger0714 <rschleyhahn@uiowa.edu>
2020-11-03 19:04:56 -0600

    added the Nodes
    Adds the Hand_grip , Orientation_control, and wrist_control nodes

--- ROS Nodes/hand_control.py
+++ ROS Nodes/hand_control.py
@@ -3,31 +3,31 @@ import rospy
 import time
 from std_msgs.msg import Int32
 
-hand_grip = 0
+grip_percent = 0
 w_angle = 90
-orientation = 0
+orientation_degree = 0
 
-def hand_grip_callback(data):
-    global hand_grip
+def grip_percent_callback(data):
+    global grip_percent
     print("Target Grip Position: ")
-    hand_grip = data.data
-    print(hand_grip)
+    grip_percent = data.data
+    print(grip_percent)
 def w_angle_callback(data):
     global w_angle
     print("Target wrist angle Postion: ")
     w_angle = data.data
     print(w_angle)
-def orientation_callback(data):
-    global orientation
+def orientation_degree_callback(data):
+    global orientation_degree
     print("Target Orientation: ")
-    orientation = data.data
-    print(orientation)
+    orientation_degree = data.data
+    print(orientation_degree)
 
 def talker():
     global hand_grip
     global w_angle
-    global orientation
-    pub_hand_control = rospy.Publisher('hand_control',Int32,queue_size=10)
+    global orientation_degree
+    pub_hand_grip = rospy.Publisher('hand_grip',Int32,queue_size=10)
     pub_wrist_control = rospy.Publihser('wrist_control',Int32,queue_size=10)
     pub_orientation_control = rospy.Publisher('orientation_control',Int32,queue_size=10)
 
@@ -38,9 +38,8 @@ def talker():
     while(pub_hand_control.get_num_connections()==0 or pub_wrist_control.get_num_connections()==0 or pub_orientation_control.get_num_connections()==0):
         print("No connection!")
 
-    rospy.Subscriber('hand_grip',Int32,hand_grip_callback)
+    rospy.Subscriber('grip_percent',Int32,grip_percent_callback)
     rospy.Subscriber('w_angle',Int32,w_angle_callback)
-    rospy.Subscriber('orientation',Int32,orientation_callback)
+    rospy.Subscriber('orientation_degree',Int32,orientation_degree_callback)
 
     while not rospy.is_shutdown():
-        
