2d6d3f0edc97bbce0e35435cef4f8698232152bb
Kyle Lastine <kylejlastine@gmail.com>
2020-12-01 20:07:38 -0600

    updated ik2

--- Test_scripts/uirobotics_ik_2.0.py
+++ Test_scripts/uirobotics_ik_2.0.py
@@ -19,44 +19,44 @@ def ik(xPos,yPos,seg1 = 1, seg2 = 1):
 
     count = 0
 
-    running = True
-    while running:
+    #running = True
+    #while running:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+        #for event in pygame.event.get():
+            #if event.type == pygame.QUIT:
+                #running = False
 
-        screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255))
 
-        pygame.draw.circle(screen,(66,135,245),(int(screen_width/2),int(screen_height/2)),6)
+    pygame.draw.circle(screen,(66,135,245),(int(screen_width/2),int(screen_height/2)),6)
 
-        pygame.draw.circle(screen,(199,38,81),(int(screen_center[0]+xPos*100),int(screen_center[1]-yPos*100)),6)
+    pygame.draw.circle(screen,(199,38,81),(int(screen_center[0]+xPos*100),int(screen_center[1]-yPos*100)),6)
 
 
-        seg1Pos = (int(screen_center[0] + 100*seg1*math.cos(lowerAngle)),int(screen_center[1] - 100*seg1*math.sin(lowerAngle)))
+    seg1Pos = (int(screen_center[0] + 100*seg1*math.cos(lowerAngle)),int(screen_center[1] - 100*seg1*math.sin(lowerAngle)))
 
-        seg2Pos = (int(100*seg2*math.cos(upperAngle+lowerAngle)+seg1Pos[0]),int(seg1Pos[1]-100*seg2*math.sin(upperAngle+lowerAngle)))
+    seg2Pos = (int(100*seg2*math.cos(upperAngle+lowerAngle)+seg1Pos[0]),int(seg1Pos[1]-100*seg2*math.sin(upperAngle+lowerAngle)))
 
-        pygame.draw.circle(screen,(227, 54, 224),seg1Pos,6)
+    pygame.draw.circle(screen,(227, 54, 224),seg1Pos,6)
 
-        pygame.draw.circle(screen,(56, 219, 50),seg2Pos,6)
+    pygame.draw.circle(screen,(56, 219, 50),seg2Pos,6)
 
-        if count == 0:
-            print((int(screen_center[0]+xPos*100),int(screen_center[1]-yPos*100)))
-            print(seg1Pos,seg2Pos)
-            count = count + 1
+    if count == 0:
+        print((int(screen_center[0]+xPos*100),int(screen_center[1]-yPos*100)))
+        print(seg1Pos,seg2Pos)
+        count = count + 1
 
-        pygame.draw.line(screen,0,screen_center,seg1Pos,4)
+    pygame.draw.line(screen,0,screen_center,seg1Pos,4)
 
-        pygame.draw.line(screen,0,seg1Pos,seg2Pos,4)
+    pygame.draw.line(screen,0,seg1Pos,seg2Pos,4)
 
         #pygame.draw.line(screen,0,screen_center,(int(screen_center[0]+xPos*100),int(screen_center[1]-yPos*100)),4)
 
         #pygame.display.flip()
 
-        pygame.image.save(screen,"arm_image.png")
+    pygame.image.save(screen,"arm_image.png")
 
-        running = False
+        #running = False
 
 
     pygame.quit()
