df0a93e66176aa11b757c8a40396fda2fc620e9f
Kyle Lastine <kylejlastine@gmail.com>
2020-12-07 17:23:06 -0600

    Update hand_control.py

--- ROS Nodes/hand_control.py
+++ ROS Nodes/hand_control.py
@@ -11,7 +11,7 @@ c_w_angle = 0
 c_orientation_degree = 0
 
 def grip_percent_callback(data):
-    global grip_percent
+    global t_grip_percent
     print("Target Grip Position: ")
     if data.data > 100:
         t_grip_percent = 100
@@ -21,7 +21,7 @@ def grip_percent_callback(data):
         t_grip_percent = data.data
     print(grip_percent)
 def w_angle_callback(data):
-    global w_angle
+    global t_w_angle
     print("Target wrist angle Postion: ")
     if data.data > 90:
         t_w_angle = 90
@@ -31,7 +31,7 @@ def w_angle_callback(data):
         t_w_angle = data.data
     print(w_angle)
 def orientation_degree_callback(data):
-    global orientation_degree
+    global t_orientation_degree
     print("Target Orientation: ")
     if data.data > 90:
         t_orientation_degree = 90
@@ -45,6 +45,10 @@ def talker():
     global t_grip_percent
     global t_w_angle
     global t_orientation_degree
+    global c_grip_percent
+    global c_w_angle
+    global c_orientation_degree
+    
     pub_hand_grip = rospy.Publisher('hand_grip',Int32,queue_size=10)
     pub_wrist_a = rospy.Publisher('wrist_a',Int32,queue_size=10)
     pub_wrist_b = rospy.Publisher('wrist_b',Int32,queue_size=10)
