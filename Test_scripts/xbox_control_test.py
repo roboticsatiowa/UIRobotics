24b1ce7efff450b57371496eb91d9c8015bada2e
Kyle Lastine <kylejlastine@gmail.com>
2021-02-09 19:39:47 -0600

    Update xbox_control_test.py

--- Test_scripts/xbox_control_test.py
+++ Test_scripts/xbox_control_test.py
@@ -1,10 +1,12 @@
 from inputs import get_gamepad
+from time import sleep
 
-xDiff = 0
-yDiff = 0
-x = 0
-y = 0 
-b = 0
+x_left = 0
+y_left = 0
+y_right = 0
+x_arm = 0
+y_arm = 0
+z_arm = 0
 mode = 0 #0 is drive, 1 is arm, 2 is claw
 rightTrig = 0
 leftTrig = 0
@@ -12,33 +14,41 @@ while True:
     events = get_gamepad()
     
     for event in events:
-        #print(event.ev_type, event.code, event.state)
+        print(event.ev_type, event.code, event.state)
         if event.code == "BTN_WEST": #Switch to drive mode
            mode = 0
         elif event.code == "BTN_NORTH": #Switch to arm mode
            mode = 1
         elif event.code == "BTN_EAST": #Switch to claw mode
-           mode = 2 
-        if event.code == "ABS_X" and -100 < xDiff + event.state/10000 < 100:
-            xDiff = event.state/10000
-        elif event.code == "ABS_Y" and -100 < yDiff + event.state/10000 < 100:
-            yDiff = event.state/10000
+           mode = 2
+           
+        if event.code == "ABS_X":
+            x_left = event.state
+        elif event.code == "ABS_Y":
+            y_left = event.state
+        elif event.code == "ABS_RY":
+            y_right = event.state
         
         if event.code == "RT": #Call Program for Right Trigger
             rightTrig = 1
         elif event.code == "LT": #Call Program for Left Trigger
             leftTrig = 1
-       
-        if mode == 0:
-            #Run Mode 1
-        elif mode == 1:
-            #Run Mode 2
-        elif mode == 2:
-            #Run Mode 3
-       
-        
+
         
 
-    x += xDiff
-    y += yDiff
-    print(x,y)
+       
+        print(mode)
+        
+
+    if mode == 0:
+        #Run Mode 1
+        print(mode)
+    elif mode == 1:
+        x_arm += x_left
+        y_arm += y_left
+        z_arm += y_right
+    elif mode == 2:
+        #Run Mode 3
+        print(mode)
+    
+    sleep(.05)
