eef402acfa87c06c6d9b70973114916ba3c68c0d
Kyle Lastine <kylejlastine@gmail.com>
2021-02-09 19:01:20 -0600

    Update xbox_control_test.py

--- UI Robotics Testing/xbox_control_test.py
+++ UI Robotics Testing/xbox_control_test.py
@@ -5,24 +5,29 @@ yDiff = 0
 x = 0
 y = 0 
 b = 0
-mode = 0
-righTrig = 0
+mode = 0 #0 is drive, 1 is arm, 2 is claw
+rightTrig = 0
 leftTrig = 0
 while True:
     events = get_gamepad()
     
     for event in events:
         #print(event.ev_type, event.code, event.state)
+        if event.code == "BTN_WEST": #Switch to drive mode
+           mode = 0
+        elif event.code == "BTN_NORTH": #Switch to arm mode
+           mode = 1
+        elif event.code == "BTN_EAST": #Switch to claw mode
+           mode = 2 
         if event.code == "ABS_X" and -100 < xDiff + event.state/10000 < 100:
             xDiff = event.state/10000
         elif event.code == "ABS_Y" and -100 < yDiff + event.state/10000 < 100:
             yDiff = event.state/10000
-        elif event.code == "ABS_B": #Call Program for b:
-           mode = 2 
-        elif event.code == "RT": #Call Program for Right Trigger
-            #Output
+        
+        if event.code == "RT": #Call Program for Right Trigger
+            rightTrig = 1
         elif event.code == "LT": #Call Program for Left Trigger
-            #Output
+            leftTrig = 1
        
         if mode == 0:
             #Run Mode 1
