c03686ac7fed3c4bc486cbe262df30b0c65b4c89
Kyle Lastine <kylejlastine@gmail.com>
2021-02-16 19:54:43 -0600

    Update controller_driver.py

--- ROS Nodes/controller_driver.py
+++ ROS Nodes/controller_driver.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
+from inputs import get_gamepad
+from time import sleep
 
 def talker():
+
+    x_left = 0
+    y_left = 0
+    y_right = 0
+    x_arm = 0
+    y_arm = 0
+    z_arm = 0
+    t_hand = 0
+    r_hand = 0
+    o_hand = 0
+    mode = 0 #0 is drive, 1 is arm, 2 is claw
+    rightTrig = 0
+    leftTrig = 0
 
     #create publishers
 
@@ -16,6 +31,34 @@ def talker():
     print("Talker initialized!")
 
     while not rospy.is_shutdown():
+
+        events = get_gamepad()
+
+        for event in events
+            #print(event.ev_type, event.code, event.state)
+            if event.code == "BTN_WEST": #Switch to drive mode
+               mode = 0
+               print("Drive mode")
+            elif event.code == "BTN_NORTH": #Switch to arm mode
+               mode = 1
+               print("Arm mode")
+            elif event.code == "BTN_EAST": #Switch to claw mode
+               mode = 2
+               print("Claw mode")
+               
+            if event.code == "ABS_X":
+                x_left = event.state
+            elif event.code == "ABS_Y":
+                y_left = event.state
+                print(y_left)
+            elif event.code == "ABS_RY":
+                y_right = event.state
+            
+            if event.code == "RT": #Call Program for Right Trigger
+                rightTrig = 1
+            elif event.code == "LT": #Call Program for Left Trigger
+                leftTrig = 1
+        
         #publish
         rate.sleep()
 
