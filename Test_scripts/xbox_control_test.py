5a3545712ab17488ee2b896e86c4e18e7f09950d
Kyle Lastine <kylejlastine@gmail.com>
2021-02-16 18:46:10 -0600

    Update xbox_control_test.py

--- Test_scripts/xbox_control_test.py
+++ Test_scripts/xbox_control_test.py
@@ -11,16 +11,34 @@ mode = 0 #0 is drive, 1 is arm, 2 is claw
 rightTrig = 0
 leftTrig = 0
 while True:
+
+    if mode == 0:
+        #Run Mode 1
+        print(mode)
+    elif mode == 1:
+        x_arm += x_left
+        y_arm += y_left
+        z_arm += y_right
+        print(x_arm,y_arm,z_arm)
+    elif mode == 2:
+        #Run Mode 3
+        print(mode)
+    
+    sleep(.05)
+    
     events = get_gamepad()
     
     for event in events:
-        print(event.ev_type, event.code, event.state)
+        #print(event.ev_type, event.code, event.state)
         if event.code == "BTN_WEST": #Switch to drive mode
            mode = 0
+           print("Drive mode")
         elif event.code == "BTN_NORTH": #Switch to arm mode
            mode = 1
+           print("Arm mode")
         elif event.code == "BTN_EAST": #Switch to claw mode
            mode = 2
+           print("Claw mode")
            
         if event.code == "ABS_X":
             x_left = event.state
@@ -37,18 +55,7 @@ while True:
         
 
        
-        print(mode)
+        #print(mode)
         
 
-    if mode == 0:
-        #Run Mode 1
-        print(mode)
-    elif mode == 1:
-        x_arm += x_left
-        y_arm += y_left
-        z_arm += y_right
-    elif mode == 2:
-        #Run Mode 3
-        print(mode)
     
-    sleep(.05)
