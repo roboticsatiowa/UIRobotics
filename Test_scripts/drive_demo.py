1a861daa002cbb61101b8433ba1a1be6045504e3
Kyle Lastine <kylejlastine@gmail.com>
2021-08-30 15:43:49 -0500

    Update drive_demo.py

--- Test_scripts/drive_demo.py
+++ Test_scripts/drive_demo.py
@@ -9,6 +9,10 @@ pca = adafruit_pca9685.PCA9685(i2c)
 pca.frequency = 1000
 left = pca.channels[0]
 right = pca.channels[1]
+left_center = pca.channels[2]
+right_center = pca.channels[3]
+left_value = 0
+right_value = 0
 
 print("Initialized!")
 
@@ -17,10 +21,18 @@ while True:
 
     for event in events:
         if event.code == "ABS_Y":
-            value = event.state + 2**15 - 1
-            left.duty_cycle = value
-            print("Left wheels set to: "+value)
+            left_value = event.state + 2**15 - 1
+            print("Left wheels set to: "+left_value)
         elif event.code == "ABS_RY":
-            value = event.state + 2**15 - 1
-            right.duty_cycle = value
-            print("Right wheels set to: "+value)
+            right_value = event.state + 2**15 - 1
+            print("Right wheels set to: "+right_value)
+    if(left_value*right_value>=0):
+        left.duty_cycle = left_value
+        right.duty_cycle = right_value
+        left_center.duty_cycle = left_value
+        right_center.duty_cycle = right_value
+    else:
+        left.duty_cycle = left_value
+        right.duty_cycle = right_value
+        left_center.duty_cycle = int(left_value*0.712)
+        right_center.duty_cycle = int(right_value*0.712)
