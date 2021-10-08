3b0387b09e4f878468cb153d47b24ba7b1872ee2
Will Martin <wcmartin01@gmail.com>
2021-10-07 20:02:35 -0500

    main window

--- gui/main.py
+++ gui/main.py
@@ -1,24 +1,59 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMainWindow, QStatusBar, QToolBar
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
-def window():
-    app = QApplication(sys.argv)
-    widget = QWidget()
+# def window():
+#     app = QApplication(sys.argv)
+#     widget = QWidget()
+#
+#     widget.setGeometry(0,0,1200,800)
+#     widget.setWindowTitle("Runtime GUI")
+#
+#     textLabel = QLabel(widget)
+#     textLabel.setText("Hello World!")
+#     textLabel.move(110,85)
+#
+#     button1 = QPushButton(widget)
+#     button1.setText("Button1")
+#     button1.move(64,32)
+#
+#
+#     widget.show()
+#     sys.exit(app.exec_())
+#
+# if __name__ == '__main__':
+#    window()
 
-    textLabel = QLabel(widget)
-    textLabel.setText("Hello World!")
-    textLabel.move(110,85)
+import sys
 
-    button1 = QPushButton(widget)
-    button1.setText("Button1")
-    button1.move(64,32)
+class Window(QMainWindow):
+    """Main Window."""
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent)
+        self.setWindowTitle('QMainWindow')
+        self.setCentralWidget(QLabel("I'm the Central Widget"))
+        self._createMenu()
+        self._createToolBar()
+        self._createStatusBar()
 
-    widget.setGeometry(50,50,320,200)
-    widget.setWindowTitle("PyQt5 Example")
-    widget.show()
-    sys.exit(app.exec_())
+    def _createMenu(self):
+        self.menu = self.menuBar().addMenu("&Menu")
+        self.menu.addAction('&Exit', self.close)
+
+    def _createToolBar(self):
+        tools = QToolBar()
+        self.addToolBar(tools)
+        tools.addAction('Exit', self.close)
+
+    def _createStatusBar(self):
+        status = QStatusBar()
+        status.showMessage("I'm the Status Bar")
+        self.setStatusBar(status)
 
 if __name__ == '__main__':
-   window()
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
