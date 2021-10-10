cf641b7e07b03ae306ee520cc0c904ac07465434
charlie-killian <91706357+charlie-killian@users.noreply.github.com>
2021-10-10 13:54:44 -0500

    Taking valid input coordinates

--- Test_scripts/generateMap.py
+++ Test_scripts/generateMap.py
@@ -3,6 +3,39 @@ from urllib.parse import urlencode, quote_plus
 import json
 import webbrowser
 
+def ifValidLatitude(x):
+    try:
+        x = float(x)
+    except:
+        if not isinstance(x, float):
+            print("Invalid input")
+            return False
+    if x > 90 or x < -90:
+        print("Invalid input")
+        return False
+    return True
+
+def ifValidLongitude(x):
+    try:
+        x = float(x)
+    except:
+        if not isinstance(x, float):
+            print("Invalid input")
+            return False
+    if x > 180 or x < -180:
+        print("Invalid input")
+        return False
+    return True
+# 10/07/2021 Charlie - taking input GPS coordinates and only accepting floats
+
+lat = input("Enter latitude: ")
+while not ifValidLatitude(lat):
+    lat = input("Enter latitude: ")
+
+lng = input("Enter longitude: ")
+while not ifValidLongitude(lng):
+    lng = input("Enter longitude: ")
+# 10/10/2021 Charlie - finished taking in GPS coordinates with valid inputs
 
 def getMapImage(lat, lng):
     urlbase = "http://maps.google.com/maps/api/staticmap?"
@@ -12,7 +45,5 @@ def getMapImage(lat, lng):
     mapURL = urlbase+args
     urlretrieve(mapURL, 'googlemap.png')
 
-
-getMapImage(38.266,-110.719)
-
-
+getMapImage(lat, lng)
+# getMapImage(38.266,-110.719)
