8c0038724b844bf4e3b69c69b2f7a3ee17b85e8e
lausberger <74087830+lausberger@users.noreply.github.com>
2021-10-21 19:17:36 -0500

    left_wheels node subscribes to right wheel topic
    this was written in a browser so if it doesn't work feel free to torch it
    
    the idea is to allow the left wheels node to know when the right wheels are turning via rightCallback

--- ROS Nodes/left_wheels.py
+++ ROS Nodes/left_wheels.py
@@ -4,11 +4,15 @@ from std_msgs.msg import Int32
 
 def callback(data):
     rospy.loginfo(data.data)
+    
+def rightCallback(rightData):
+    rospy.loginfo(rightData.data)
 
 def left_wheels():
 
     rospy.init_node('left_wheels')
     rospy.Subscriber('left_wheels', Int32, callback)
+    rospy.Subscriber('right_wheels', Int32, rightCallback)
     rospy.spin()
 
 if __name__ == '__main__':
