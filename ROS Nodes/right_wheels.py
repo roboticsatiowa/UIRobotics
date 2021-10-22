6ee1dd082a1fd730ca54cdaaf1a21fafa86162c7
lausberger <74087830+lausberger@users.noreply.github.com>
2021-10-21 19:20:47 -0500

    Update right_wheels.py
    this was written in a browser so if it doesn't work feel free to torch it
    
    the idea is to allow the right wheels node to know when the left wheels are turning via leftCallback

--- ROS Nodes/right_wheels.py
+++ ROS Nodes/right_wheels.py
@@ -4,11 +4,15 @@ from std_msgs.msg import Int32
 
 def callback(data):
     rospy.loginfo(data.data)
+    
+def leftCallback(leftData):
+    rospy.loginfo(leftData.data)
 
 def right_wheels():
 
     rospy.init_node('right_wheels')
     rospy.Subscriber('right_wheels', Int32, callback)
+    rospy.Subscriber('left_wheels', Int32, leftCallback)
     rospy.spin()
 
 if __name__ == '__main__':
