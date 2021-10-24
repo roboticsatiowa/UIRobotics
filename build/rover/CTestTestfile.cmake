291e8b45d2fec3213e91bacceb11be10b3fcece8
WillMartin7 <wcmartin01@gmail.com>
2021-10-24 14:11:32 -0500

    remove files i didnt want

--- build/rover/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/will/robotics_ws/src/rover
-# Build directory: /home/will/robotics_ws/build/rover
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
