690768305411697253fc8f08547ede8b6e6a257c
WillMartin7 <wcmartin01@gmail.com>
2021-11-28 14:40:46 -0600

    .gitignore is now working

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/will/robotics_ws/src
-# Build directory: /home/will/robotics_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("rover")
