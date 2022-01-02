e122aa9f0f4ed6d1fe28c5129dd69f8d4570d3fa
WillMartin7 <wcmartin01@gmail.com>
2022-01-01 19:38:27 -0600

    sub to realsense color image, pub a better way

--- src/rover/src/gui.py
+++ src/rover/src/gui.py
@@ -5,15 +5,17 @@ from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QVBoxLayout
-
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot, Qt
 
 from functools import partial
+import cv2
+import numpy as np
 
 import sys
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage
 
 
 class Window(QMainWindow):
@@ -21,6 +23,10 @@ class Window(QMainWindow):
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
+
+        self.pub_mode = rospy.Publisher('mode', String, queue_size=10)
+        rospy.Subscriber('/camera/color/image_raw/compressed', CompressedImage, self._camera_color_callback)
+
         self.setWindowTitle('Robotics at Iowa GUI')
         self.setFixedSize(1000, 500)
 
@@ -47,16 +53,19 @@ class Window(QMainWindow):
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)
 
+    def _camera_color_callback(self, data):
+        np_frame = np.fromstring(data.data, np.uint8)
+        self.frame = cv2.imdecode(np_frame, cv2.IMREAD_COLOR)
 
     def sendAuto(self):
         msg="AUTO"
         if not rospy.is_shutdown():
-            pub_mode.publish(msg)
+            self.pub_mode.publish(msg)
 
     def sendTeleop(self):
         msg="TELEOP"
         if not rospy.is_shutdown():
-            pub_mode.publish(msg)
+            self.pub_mode.publish(msg)
 
 class Ctrl:
     """Controller class."""
@@ -73,9 +82,9 @@ class Ctrl:
 
 if __name__ == '__main__':
     try:
-        pub_mode=rospy.Publisher('mode', String, queue_size=10)
+        # pub_mode=rospy.Publisher('mode', String, queue_size=10)
         rospy.init_node('gui')
-        rate = rospy.Rate(10) # currently not used
+        # rate = rospy.Rate(10) # currently not used
 
         app = QApplication(sys.argv)
         win = Window()
