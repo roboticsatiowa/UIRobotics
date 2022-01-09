2b74ed698c0f3e9a7ad4e5b8bec38ebcb90090d0
WillMartin7 <wcmartin01@gmail.com>
2022-01-08 23:14:10 -0600

    basic usb camera stream

--- src/rover/src/usb_camera.py
+++ src/rover/src/usb_camera.py
@@ -6,8 +6,20 @@ from cv_bridge import CvBridge
 import cv2
 
 def publish_message():
-    print("hello world")
+    pub_video = rospy.Publisher('usb_video_frame', Image, queue_size=10)
+    rospy.init_node('usb_camera', anonymous=True)
+    rate = rospy.Rate(10) # 10 FPS
 
+    cap = cv2.VideoCapture(0) # 0 -> webcam number
+    br = CvBridge()
+
+    while not rospy.is_shutdown():
+        ret, frame = cap.read()
+        
+        if ret == True:
+            pub_video.publish(br.cv2_to_imgmsg(frame))
+        
+        rate.sleep()
 
 if __name__ == "__main__":
     try:
