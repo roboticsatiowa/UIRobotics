079bec99248dbc45fdca441c2984ad33e400fa49
charlie-killian <91706357+charlie-killian@users.noreply.github.com>
2022-02-17 20:24:06 -0600

    lat and lng buttons functional

--- src/rover/src/nestedLayout.py
+++ src/rover/src/nestedLayout.py
@@ -26,10 +26,10 @@ class Window(QMainWindow):
         self.bottom_right_layout = QVBoxLayout()
         self.gps_layout = QVBoxLayout()
         self.lat_lng_layout = QHBoxLayout()
-        
+
 
         #nest layouts
-        
+
         self.main_layout.addLayout(self.top_layout)
         self.main_layout.addLayout(self.bottom_layout)
         self.bottom_layout.addLayout(self.bottom_left_layout)
@@ -38,14 +38,14 @@ class Window(QMainWindow):
               #self.bottom_middle_layout.addLayout(self.timer_button_layout)
         self.bottom_right_layout.addLayout(self.gps_layout)
         self.bottom_right_layout.addLayout(self.lat_lng_layout)
-    
+
 
          # add to gps_layout
 
         self.central_widget = QWidget()
         self.central_widget.setLayout(self.main_layout)
         self.setCentralWidget(self.central_widget)
-        
+
         self._create_slider()
         self._create_gps_buttons()
         self._create_video_feeds()
@@ -61,6 +61,7 @@ class Window(QMainWindow):
         self.vid2.setStyleSheet("border: 3px solid orange")
         self.top_layout.addWidget(self.vid1)
         self.top_layout.addWidget(self.vid2)
+
     def _create_mode_buttons(self):
         # creating start button
         self.start_button = QPushButton("Start", self)
@@ -78,13 +79,14 @@ class Window(QMainWindow):
         self.bottom_left_layout.addWidget(self.start_button)
         self.bottom_left_layout.addWidget(self.pause_button)
         self.bottom_left_layout.addWidget(self.reset_button)
+
     def _create_timer(self):
-        
         self.label = QLabel("//TIMER//", self)
         self.label.setStyleSheet("border : 3px solid black")
         self.label.setFont(QFont('Times', 15))
         self.bottom_middle_layout.addWidget(self.label)
         self.bottom_middle_layout.addLayout(self.timer_button_layout)
+
     def _create_timer_buttons(self):
          # creating push button to get time in seconds
          self.button = QPushButton("Set time", self)
@@ -106,21 +108,20 @@ class Window(QMainWindow):
          self.timer_button_layout.addWidget((self.start_button), 0,1)
          self.timer_button_layout.addWidget((self.pause_button), 1,0)
          self.timer_button_layout.addWidget((self.reset_button), 1,1)
+
     def _create_gps_buttons(self):
-        self.buttons = {}
-        buttons = {'LAT': (0, 0), 'LNG': (0,1)}
+        #creating latitude input text box
+        latitudeButton = QPushButton("Set Latitude", self)
+        latitudeButton.clicked.connect(self.getLatitude)
+        latitudeButton.setFont(QFont('Times', 11))
 
-        # create the buttons and add them to the grid layout
-        for btnText, pos in buttons.items():
-            # create button
-            self.buttons[btnText] = QPushButton(btnText)
-            self.buttons[btnText].setFixedSize(150, 40)
+        #creating longitude input text box
+        longitudeButton = QPushButton("Set Longitude", self)
+        longitudeButton.clicked.connect(self.getLongitude)
+        longitudeButton.setFont(QFont('Times', 11))
 
-            # connect button to publisher (currenly just publishes button text)
-            self.buttons[btnText].clicked.connect(lambda state, msg=btnText: self._send_mode(msg))
-
-            # add button to button layout
-            self.lat_lng_layout.addWidget(self.buttons[btnText])
+        self.lat_lng_layout.addWidget(latitudeButton)
+        self.lat_lng_layout.addWidget(longitudeButton)
 
     def _create_slider(self):
         self.slider = QSlider(Qt.Horizontal, self)
@@ -131,6 +132,7 @@ class Window(QMainWindow):
         self.slider.setTickInterval(1)
         # self.slider.setGeometry(700, 600, 200, 50)
         #self.slider.valueChanged.connect(self.sliderValueChanged)
+        self.slider.valueChanged.connect(self.sliderValueChanged)
         self.bottom_right_layout.addWidget(self.slider)
 
     def _create_gps(self):
@@ -148,7 +150,31 @@ class Window(QMainWindow):
         self.label3.setAlignment(Qt.AlignCenter)
         #self.gps_layout.addWidget(self.label3)
         self.gps_layout.addWidget(self.label3)
-    
+
+
+    def getMapImage(self, lat, lng, zoom):
+        urlbase = "http://maps.google.com/maps/api/staticmap?"
+        GOOGLEAPIKEY = "AIzaSyCHD0L-s_gWE6VTNumgn1TMCEhiDTEok_U"
+        args = "center={},{}&zoom={}&size={}x{}&format=gif&maptype={}&markers=color:red|size:small|{},{}|".format(lat,lng,zoom,400,400,"hybrid",lat,lng)
+        args = args + "&key=" + GOOGLEAPIKEY
+        mapURL = urlbase+args
+        urlretrieve(mapURL, 'googlemap.png')
+        img = QPixmap('googlemap.png')
+        return img
+
+    def getLatitude(self):
+        self.latitude, ok = QInputDialog.getDouble(self, "Latitude", "Enter Latitude", 0, -90, 90)
+    #method to get longitude
+    def getLongitude(self):
+        self.longitude, ok = QInputDialog.getDouble(self, "Longitude", "Enter Longitude", 0, -180, 180)
+
+    #slider value change method
+    def sliderValueChanged(self):
+        self.getMapImage(self.latitude, self.longitude, self.slider.value())
+        self.label3.clear()
+        GPSpixmap = QPixmap('googlemap.png')
+        self.label3.setPixmap(GPSpixmap)
+
     def start_action(self):
 		# making flag true
         self.start = True
@@ -161,6 +187,7 @@ class Window(QMainWindow):
 
 		# making flag false
         self.start = False
+
     def reset_action(self):
 
 		# making flag false
@@ -171,6 +198,7 @@ class Window(QMainWindow):
 
 		# setting label text
         self.label.setText("//TIMER//")
+
     def get_seconds(self):
 
 		# making flag false
@@ -189,6 +217,7 @@ class Window(QMainWindow):
             self.label.setText(str(second))
 
             self.label.setText(str(hrs)+":"+str(mins)+":"+str(second))
+
     def start_action(self):
 		# making flag true
         self.start = True
@@ -232,7 +261,7 @@ class Window(QMainWindow):
         self.bottom_left_layout.addWidget(self.stopButton)
         self.bottom_left_layout.addWidget(self.autoButton)
         self.bottom_left_layout.addWidget(self.manualButton)
-    
+
     def showTime(self):
 
 		# checking if flag is true
@@ -255,8 +284,8 @@ class Window(QMainWindow):
 
 			# showing text
             self.label.setText(text)
-    
-        
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
