9ac346ddf2b1382d39327bcc144d48af03aa7439
Kyle Lastine <kylejlastine@gmail.com>
2022-02-23 16:19:46 -0600

    add hardware interface to wrist nodes

--- src/rover/src/wrist_a.py
+++ src/rover/src/wrist_a.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
+import board
+import busio
+import adafruit_pca9685
 
-def callback(data):
-    rospy.loginfo(data.data)
+i2c = busio.I2C(board.SCL,board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+pca.frequency = 1000
+w_channel = pca.channels[0]
+
+def callback_a(data):
+    w_channel.duty_cycle - data.data
 
 def wrist_a():
     rospy.init_node('wrist_a')
-    rospy.Subscriber('wrist_a',Int32,callback)
+    rospy.Subscriber('wrist_a',Int32,callback_a)
     rospy.spin()
 
 if __name__ == '__main__':
--- src/rover/src/wrist_b.py
+++ src/rover/src/wrist_b.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
+import board
+import busio
+import adafruit_pca9685
 
+i2c = busio.I2C(board.SCL,board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+pca.frequency = 1000
+w_channel = pca.channels[1]
 
-def callback(data):
-    rospy.loginfo(data.data)
+def callback_b(data):
+    w_channel.duty_cycle - data.data
 
 def wrist_b():
     rospy.init_node('wrist_b')
-    rospy.Subscriber('wrist_b',Int32,callback)
+    rospy.Subscriber('wrist_b',Int32,callback_b)
     rospy.spin()
 
 if __name__ == '__main__':
