b07618e83a18951855c82d2d0e9d7956bd07d356
WillMartin7 <wcmartin01@gmail.com>
2022-03-06 13:34:12 -0600

    gui functional

--- src/rover/src/gui.py
+++ src/rover/src/gui.py
@@ -14,6 +14,7 @@ from PyQt5.QtGui import *
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage, Image
+from urllib.request import urlopen, urlretrieve
 
 
 class Window(QMainWindow):
@@ -45,7 +46,11 @@ class Window(QMainWindow):
         self.bottom_right_layout = QVBoxLayout()
         self.gps_layout = QVBoxLayout()
         self.lat_lng_layout = QHBoxLayout()
-
+        self.bottom_right_widget = QWidget()
+        self.bottom_right_widget.setLayout(self.bottom_right_layout)
+        self.bottom_right_widget.setFixedWidth(480)
+        self.bottom_layout.addWidget(self.bottom_right_widget)
+        # self.slider_layout = QVBoxLayout()
         self.main_layout.addLayout(self.top_layout)
         self.main_layout.addLayout(self.bottom_layout)
         self.bottom_layout.addLayout(self.bottom_left_layout)
@@ -54,6 +59,10 @@ class Window(QMainWindow):
         self.bottom_right_layout.addLayout(self.gps_layout)
         self.bottom_right_layout.addLayout(self.lat_lng_layout)
         self.bottom_middle_layout.addLayout(self.timer_button_layout)
+        # self.gps_layout.addLayout(self.slider_layout)
+        self.bottom_right_layout.setStretch(2,-2) #change width?
+
+        #QSizePolicy class?
 
 
         self.central_widget = QWidget()
@@ -64,6 +73,9 @@ class Window(QMainWindow):
         self._create_modes()
         self._create_video_feeds()
         self._create_timer_components()
+        self._create_gps_buttons()
+        self._create_gps()
+        self._create_slider()
 
 
     def _create_modes(self):
@@ -82,6 +94,73 @@ class Window(QMainWindow):
         self.bottom_left_layout.addWidget(self.stopButton)
         self.bottom_left_layout.addWidget(self.autoButton)
         self.bottom_left_layout.addWidget(self.manualButton)
+
+    def _create_gps_buttons(self):
+        #creating latitude input text box
+        latitudeButton = QPushButton("Set Latitude", self)
+        latitudeButton.clicked.connect(self.getLatitude)
+        latitudeButton.setFont(QFont('Times', 11))
+
+        #creating longitude input text box
+        longitudeButton = QPushButton("Set Longitude", self)
+        longitudeButton.clicked.connect(self.getLongitude)
+        longitudeButton.setFont(QFont('Times', 11))
+
+        self.lat_lng_layout.addWidget(latitudeButton)
+        self.lat_lng_layout.addWidget(longitudeButton)
+
+    def _create_slider(self):
+        self.slider = QSlider(Qt.Horizontal, self)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(24)
+        self.slider.setValue(12)
+        self.slider.setTickPosition(QSlider.TicksBelow)
+        self.slider.setTickInterval(1)
+        self.slider.setGeometry(400, 30, 800, 1000)
+        # self.slider.setGeometry(700, 600, 200, 50)
+        #self.slider.valueChanged.connect(self.sliderValueChanged)
+        self.slider.valueChanged.connect(self.sliderValueChanged)
+        self.bottom_right_layout.addWidget(self.slider)
+
+    def _create_gps(self):
+        # label3 = QLabel("GPS", self)
+        # label3.setStyleSheet("border: 3px solid orange")
+        # label3.setFont(QFont('Times', 15))
+        # label3.setAlignment(Qt.AlignCenter)
+
+        self.label3 = QLabel("GPS", self)
+        # self.label3.setGeometry(700, 400, 200,200)
+        # GPSpixmap = QPixmap('googlemap.png')
+        #label3.setPixmap(GPSpixmap)
+        self.label3.setStyleSheet("border: 3px solid orange")
+        self.label3.setFont(QFont('Times', 15))
+        self.label3.setAlignment(Qt.AlignCenter)
+        #self.gps_layout.addWidget(self.label3)
+        self.gps_layout.addWidget(self.label3)
+
+
+    def getMapImage(self, lat, lng, zoom):
+        urlbase = "http://maps.google.com/maps/api/staticmap?"
+        GOOGLEAPIKEY = "AIzaSyCHD0L-s_gWE6VTNumgn1TMCEhiDTEok_U"
+        args = "center={},{}&zoom={}&size={}x{}&format=gif&maptype={}&markers=color:red|size:small|{},{}|".format(lat,lng,zoom,400,400,"hybrid",lat,lng)
+        args = args + "&key=" + GOOGLEAPIKEY
+        mapURL = urlbase+args
+        urlretrieve(mapURL, 'googlemap.png')
+        img = QPixmap('googlemap.png')
+        return img
+
+    def getLatitude(self):
+        self.latitude, ok = QInputDialog.getDouble(self, "Latitude", "Enter Latitude", 0, -90, 90)
+    #method to get longitude
+    def getLongitude(self):
+        self.longitude, ok = QInputDialog.getDouble(self, "Longitude", "Enter Longitude", 0, -180, 180)
+
+    #slider value change method
+    def sliderValueChanged(self):
+        self.getMapImage(self.latitude, self.longitude, self.slider.value())
+        self.label3.clear()
+        GPSpixmap = QPixmap('googlemap.png')
+        self.label3.setPixmap(GPSpixmap)
 
 
     def _create_video_feeds(self):
