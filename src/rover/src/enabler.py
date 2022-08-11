f29c1e45934ea1288b9a976458baa9a8b63cb620
Will Martin <wcmartin01@gmail.com>
2022-08-11 02:37:26 +0000

    enabler cleanup

--- src/rover/src/enabler.py
+++ src/rover/src/enabler.py
@@ -5,28 +5,36 @@ import rospy
 from std_msgs.msg import  Bool
 import RPi.GPIO as GPIO
 
-output_pin = 18 # 12 on Jetson Nano header
+class Enabler():
+    def __init__(self):
+        self.output_pin = 18 # 12 on Jetson Nano header
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
 
-def listener():
+        # ros pub and sub
+        rospy.Subscriber('/enabled', Bool, self.enabled_callback)
 
-    rospy.init_node('enabler')
-    rospy.Subscriber('/enabled', Bool, enabled_callback)
-    rospy.spin()
-    
-    rospy.loginfo("Cleaning GPIO pins...")
-    GPIO.cleanup()
+    def listen(self):
+        rospy.spin()
 
-def enabled_callback(data):
-    if data.data:
-        GPIO.output(output_pin, GPIO.LOW)
-    else:
-        GPIO.output(output_pin, GPIO.HIGH)
+        # cleanup on aisle GPIO
+        rospy.loginfo("Cleaning GPIO pins...")
+        GPIO.cleanup()
+
+    def enabled_callback(self, data):
+        if data.data:
+            GPIO.output(output_pin, GPIO.LOW)
+        else:
+            GPIO.output(output_pin, GPIO.HIGH)
+
 
 if __name__ == '__main__':
     try:
-        listener()
+        # initialize ros node
+        rospy.init_node('enabler')
+
+        enabler = Enabler()
+        enabler.listen()
     except rospy.ROSInterruptException:
         pass
