e752689668811c73776d7de6b076b57a43730eda
Will Martin <wcmartin01@gmail.com>
2022-08-11 03:02:39 +0000

    left and right wheels cleanup

--- src/rover/src/left_wheels.py
+++ src/rover/src/left_wheels.py
@@ -5,50 +5,49 @@ import board
 import busio
 import adafruit_pca9685
 
-i2c = busio.I2C(board.SCL,board.SDA)
-pca = adafruit_pca9685.PCA9685(i2c)
-pca.frequency = 1000
-left_outer = pca.channels[6]
-left_inner = pca.channels[9]
+class LeftWheels():
+    def __init__(self):
+        i2c = busio.I2C(board.SCL,board.SDA)
+        pca = adafruit_pca9685.PCA9685(i2c)
+        pca.frequency = 1000
+        left_outer = pca.channels[6]
+        left_inner = pca.channels[9]
 
-turning = False
+        self.mostRecentLeftData = -1
+        self.mostRecentRightData = -1
 
-mostRecentLeftData = -1
-mostRecentRightData = -1
+        # ros pub and sub
+        rospy.Subscriber('left_wheels', Int32, self.left_callback)
+        rospy.Subscriber('right_wheels', Int32, self.right_callback)
 
-def callback(data):
-    global turning
-    global mostRecentLeftData
-    global mostRecentRightData
-    rospy.loginfo(data.data)
-    mostRecentLeftData = data.data
-    
-    if mostRecentLeftData != -1 and mostRecentRightData != -1: # if these have been initialized
-        # if either is less than 2^15 while the other is greater than 2^15, turning is True
-        turning = (mostRecentLeftData - 32768 > 0 and mostRecentRightData - 32768 < 0) or (mostRecentLeftData - 32768 < 0 and mostRecentRightData - 32768 > 0)
-    
-    if turning:
-        left_outer.duty_cycle = int(data.data/(4/3))+8192
-        left_inner.duty_cycle = int(data.data/(4/3))+8192
-        #left_inner.duty_cycle = int((((data.data-32768)*.793)+32768)/(4/3))+8192
-    else:
-        left_outer.duty_cycle = int(data.data/(4/3))+8192
-        left_inner.duty_cycle = int(data.data/(4/3))+8192
-    
-def rightCallback(rightData):
-    #rospy.loginfo(rightData.data)
-    global mostRecentRightData
-    mostRecentRightData = rightData.data
-    
-    #if mostRecentLeftData != -1 and mostRecentRightData != -1: # if these have been initialized
-        # if either is less than 2^15 while the other is greater than 2^15, turning is True
-        #turning = (mostRecentLeftData - 32768 > 0 and mostRecentRightData - 32768 < 0) or (mostRecentLeftData - 32768 < 0 and mostRecentRightData - 32768 > 0)
+    def run(self):
+        rospy.spin()
 
-def left_wheels():
-    rospy.init_node('left_wheels')
-    rospy.Subscriber('left_wheels', Int32, callback)
-    rospy.Subscriber('right_wheels', Int32, rightCallback)
-    rospy.spin()
+    def left_callback(self, data):
+        self.mostRecentLeftData = data.data
+    
+        if self.mostRecentLeftData != -1 and self.mostRecentRightData != -1: # if these have been initialized
+            # if either is less than 2^15 while the other is greater than 2^15, turning is True
+            turning = (self.mostRecentLeftData - 32768 > 0 and self.mostRecentRightData - 32768 < 0) or (self.mostRecentLeftData - 32768 < 0 and self.mostRecentRightData - 32768 > 0)
+        
+        if turning:
+            left_outer.duty_cycle = int(data.data/(4/3))+8192
+            left_inner.duty_cycle = int(data.data/(4/3))+8192
+            #left_inner.duty_cycle = int((((data.data-32768)*.793)+32768)/(4/3))+8192
+        else:
+            left_outer.duty_cycle = int(data.data/(4/3))+8192
+            left_inner.duty_cycle = int(data.data/(4/3))+8192
+
+    def right_callback(self, data):
+        self.mostRecentRightData = rightData.data
+
 
 if __name__ == '__main__':
-    left_wheels()
+    try:
+        # initialize ros node
+        rospy.init_node('left_wheels')
+
+        left_wheels = LeftWheels()
+        left_wheels.run()
+    except rospy.ROSInterruptException:
+        pass
--- src/rover/src/right_wheels.py
+++ src/rover/src/right_wheels.py
@@ -5,52 +5,50 @@ import board
 import busio
 import adafruit_pca9685
 
-i2c = busio.I2C(board.SCL,board.SDA)
-pca = adafruit_pca9685.PCA9685(i2c)
-pca.frequency = 1000
-right_outer = pca.channels[7]
-right_inner = pca.channels[8]
+class RightWheels():
+    def __init__(self):
+        i2c = busio.I2C(board.SCL,board.SDA)
+        pca = adafruit_pca9685.PCA9685(i2c)
+        pca.frequency = 1000
+        right_outer = pca.channels[7]
+        right_inner = pca.channels[8]
 
-turning = False
+        self.mostRecentLeftData = -1
+        self.mostRecentRightData = -1
 
-mostRecentRightData = -1
-mostRecentLeftData = -1
+        # ros pub and sub
+        rospy.Subscriber('left_wheels', Int32, self.left_callback)
+        rospy.Subscriber('right_wheels', Int32, self.right_callback)
 
-def callback(data):
-    global turning
-    global mostRecentLeftData
-    global mostRecentRightData
-    rospy.loginfo(data.data)
-    mostRecentRightData = data.data
-    
-    if mostRecentRightData != -1 and mostRecentLeftData != -1: # if these have been initialized
-        # if either is less than 2^15 while the other is greater than 2^15, turning is True
-        turning = (mostRecentRightData - 32768 > 0 and mostRecentLeftData - 32768 < 0) or (mostRecentRightData - 32768 < 0 and mostRecentLeftData - 32768 > 0)
-    
-    if turning:
-        right_outer.duty_cycle = int(data.data/(4/3))+8192
-        right_inner.duty_cycle = int(data.data/(4/3))+8192
-        #right_inner.duty_cycle = int((((data.data-32768)*.793)+32768)/(4/3))+8192
-    else:
-        right_outer.duty_cycle = int(data.data/(4/3))+8192
-        right_inner.duty_cycle = int(data.data/(4/3))+8192
+    def run(self):
+        rospy.spin()
+
+    def left_callback(self, data):
+        self.mostRecentLeftData = data.data
+
+    def right_callback(self, data):
+        self.mostRecentRightData = rightData.data
+
+        if self.mostRecentLeftData != -1 and self.mostRecentRightData != -1: # if these have been initialized
+            # if either is less than 2^15 while the other is greater than 2^15, turning is True
+            turning = (self.mostRecentLeftData - 32768 > 0 and self.mostRecentRightData - 32768 < 0) or (self.mostRecentLeftData - 32768 < 0 and self.mostRecentRightData - 32768 > 0)
         
-def leftCallback(leftData):
-    global turning
-    global mostRecentLeftData
-    #rospy.loginfo(leftData.data)
-    mostRecentLeftData = leftData.data
-    
-    #if mostRecentRightData != -1 and mostRecentLeftData != -1: # if these have been initialized
-        # if either is less than 2^15 while the other is greater than 2^15, turning is True
-        #turning = (mostRecentRightData - 32768 > 0 and mostRecentLeftData - 32768 < 0) or (mostRecentRightData - 32768 < 0 and mostRecentLeftData - 32768 > 0)
+        if turning:
+            right_outer.duty_cycle = int(data.data/(4/3))+8192
+            right_inner.duty_cycle = int(data.data/(4/3))+8192
+            #left_inner.duty_cycle = int((((data.data-32768)*.793)+32768)/(4/3))+8192
+        else:
+            right_outer.duty_cycle = int(data.data/(4/3))+8192
+            right_inner.duty_cycle = int(data.data/(4/3))+8192
 
-def right_wheels():
-
-    rospy.init_node('right_wheels')
-    rospy.Subscriber('right_wheels', Int32, callback)
-    rospy.Subscriber('left_wheels', Int32, leftCallback)
-    rospy.spin()
 
 if __name__ == '__main__':
-    right_wheels()
+    try:
+        # initialize ros node
+        rospy.init_node('left_wheels')
+
+        right_wheels = RightWheels()
+        right_wheels.run()
+    except rospy.ROSInterruptException:
+        pass
+
